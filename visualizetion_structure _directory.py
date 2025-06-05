e4e13a77b8f026a4d01cec81324f4d051efa315d
Volodymyr Morhun <your.email@example.com>
2025-06-05 20:04:10 +0300

    visualization_structure_directory

--- visualizetion_structure _directory.py
+++ visualizetion_structure _directory.py
@@ -1,7 +1,26 @@
-import colorama, sys, pathlib
-def visualization_structure _directory(path="D:\Morhun_Volodymyr\Mashine_learning\data_project\goit-algo-hw-04\goit-algo-hw-04"):
-  
-    
+import sys, pathlib
+from colorama import Fore, Back, Style, init
 
+init(autoreset=True)
+
+def visualization_structure_directory(path):
+    directory = pathlib.Path(path)
+    colors=[Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
+    i=0
+    for path in directory.iterdir():
+        #for color in colors:
+       print(colors[i] + f"📁 {path}/" + Style.DIM)
+       i+=1
+    return path
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(Fore.RED + "Будь ласка, передайте шлях до директорії як аргумент.")
+        
+    else:
+        directory_path = sys.argv[1]
+        visualization_structure_directory(directory_path) 
+
+visualization_structure_directory(r"D:\Morhun_Volodymyr\Mashine_learning")
 
 
