480eed98af1f7a6c1dae58db816aaa4cb7d95cee
Volodymyr Morhun <your.email@example.com>
2025-06-05 18:12:36 +0300

    get_cats_info

--- get_cats_info.py
+++ get_cats_info.py
@@ -1,13 +1,14 @@
 def get_cats_info(path):
-    cats_info={}
+    cats_info=[]
     with open(path, 'r', encoding='utf-8') as fh:
         for line in fh:
           el=line.strip().split(",")
-          cats_info={"id": el[0], "name": el[1], "age": el[2]}
-            
-          print(cats_info)
+          cats_info_dic={"id": el[0], "name": el[1], "age": el[2]}
+          cats_info.append(cats_info_dic)
+
+    return cats_info  
+        
 
 
 
-path="Cat.txt"
-get_cats_info(path)
+print(get_cats_info("Cat.txt"))
