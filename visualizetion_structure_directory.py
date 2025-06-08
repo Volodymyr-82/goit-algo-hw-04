6fb27cd40053122acc18ec6377ea7c80b3f2ee12
Volodymyr Morhun <your.email@example.com>
2025-06-08 13:58:29 +0300

    visualization_structure_directory

--- visualizetion_structure_directory.py
+++ visualizetion_structure_directory.py
@@ -12,23 +12,15 @@ def visualization_structure(path):
         return
 
     for item in path.iterdir():
-        indent = "  " * depth  
+        indent = "└──" * depth
         if item.is_dir():
-            print(Fore.BLUE + indent +"└── " +f"📁 {item.name}")
+            print(Fore.BLUE + indent +"──" +f"📁 {item.name}")
             visualization_structure(item) 
             depth+=1
         else:
-            print(Fore.GREEN +indent +"└── "+ f"📄 {item.name}")
+            print(Fore.GREEN +indent +"──"+ f"📄 {item.name}")
     return
 
 
 
-#visualization_structure(r"D:\Morhun_Volodymyr\Mashine_learning\data_project")
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(Fore.RED + "⚠️  Вкажи шлях до директорії як аргумент.")
-        print(Fore.YELLOW + "📌 Приклад: python visualizetion_structure_directory.py D:\\MyFolder")
-    else:
-        start_path = sys.argv[1]
-        print(Fore.CYAN + f"\n📂 Структура директорії: {start_path}\n")
-        visualization_structure(start_path)
+visualization_structure(r"D:\Morhun_Volodymyr\Mashine_learning\data_project")
